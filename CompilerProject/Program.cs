993f1c0b696c70c4242fc9c6db08ac1db63c03f2
Mitchell Plunkett <mitchell.plunkett@gmail.com>
2024-07-12 12:59:54 -0400

    some more emmitter tests

--- CompilerProject/Program.cs
+++ CompilerProject/Program.cs
@@ -6,11 +6,14 @@ internal class Program
     // driver program for testing lexer
     private static void Main(string[] args)
     {
-        //string source = "IF+-123 foo*THEN/";
-        string source = File.ReadAllText("ParserTest.txt"); // Test file only. will need to make dynamic for actual program (args)
-        L.Lexer lexer = new L.Lexer(source);
-        P.Parser parser = new P.Parser(lexer);
-        parser.Program();
-        Console.ReadLine();
+        /* //string source = "IF+-123 foo*THEN/";
+         string source = File.ReadAllText("ParserTest.txt"); // Test file only. will need to make dynamic for actual program (args)
+         L.Lexer lexer = new L.Lexer(source);
+         P.Parser parser = new P.Parser(lexer);
+         parser.Program();
+         Console.ReadLine();*/
+
+        System.Diagnostics.Process.Start("Assembler.asm");
+        
     }
 }
