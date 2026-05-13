5334af310358d55824b56632238487ff702fe007
Mitchell Plunkett <mitchell.plunkett@gmail.com>
2026-05-13 10:41:29 -0400

    commit

--- Lexer/Lexer.cs
+++ Lexer/Lexer.cs
@@ -21,6 +21,7 @@ namespace Lexer
         }
 
         // shifts the char pointer to the next char in the token
+        // lalala
         public void NextChar()
         {
             this.currPos++;
